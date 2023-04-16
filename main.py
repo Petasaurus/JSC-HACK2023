6dc94bced58ca875e0befb9e115e9cf068ff362a
ktprogram <kennychelsea1@gmail.com>
2023-04-15 21:57:37 -0500

    changes

--- main.py
+++ main.py
@@ -10,17 +10,17 @@ BACKGROUND = pygame.image.load("lvl3BG (2).png")
 WHITE = (255, 255, 255)
 
 FPS = 60
-VEL = 5
+VEL = 20
 SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 40
 
 YELLOW_SPACESHIP_IMAGE = pygame.image.load(
-    os.path.join('Assets', 'kanye.png'))
+    os.path.join('Assets', 'spaceship_yellow.png'))
 YELLOW_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(YELLOW_SPACESHIP_IMAGE, (SPACESHIP_WIDTH,SPACESHIP_HEIGHT)), 90)
 RED_SPACESHIP_IMAGE = pygame.image.load(
     os.path.join('Assets', 'spaceship_red.png'))
 RED_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(RED_SPACESHIP_IMAGE, (SPACESHIP_WIDTH,SPACESHIP_HEIGHT)), 270)
 
-# this is the function where it changes the window to just a blank white screen
+# this is the background
 def draw_window(red, yellow):
     WIN.fill(WHITE)
     WIN.blit(BACKGROUND,(0,0))
